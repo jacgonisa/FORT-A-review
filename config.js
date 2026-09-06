1b4b8e29cc30e1c56577b43d8c3a412709f74968
jacgonisa <jacobgisa17@gmail.com>
2026-09-06 17:12:02 +0100

    FORT-A review app + 5-annotator split

---
+++ config.js
@@ -0,0 +1,10 @@
+// Firebase web-app config for the FORT-A review app (gitignored; ships with the deploy).
+window.firebaseConfig = {
+  apiKey: "AIzaSyD1BiwAyPQhP1vn5MXUfafas2-oJK3MY5s",
+  authDomain: "forta-5336d.firebaseapp.com",
+  projectId: "forta-5336d",
+  storageBucket: "forta-5336d.firebasestorage.app",
+  messagingSenderId: "39208173633",
+  appId: "1:39208173633:web:e745d670aedacd501f86c0",
+  measurementId: "G-2Q23PVP3KM"
+};
